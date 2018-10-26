a568f4c6f0653dcfd93c8d88028f31160dfdf988
Dina <dmrabanal@gmail.com>
2018-10-26 16:50:01 -0600

    Initial commit

---
+++ src/Building.java
@@ -0,0 +1,18 @@
+public class Building {
+
+  static final int FLOORS = 6;
+ //private int floors; this would be for all buildings
+
+  static void foo(Building build){
+
+    System.out.println(build.totalChairs());
+
+  }
+  public String totalChairs(){
+
+  }
+  public static void main(String args[]){}
+
+
+
+}
